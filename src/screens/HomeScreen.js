05f15ac78a5a727e24ccb41d3f9b80b4fc4f5319
Shawn Tschoepe <shawn.42496@gmail.com>
2020-06-23 21:36:37 -0500

    Create Name Input Screen

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -42,6 +42,12 @@ const HomeScreen = ({ navigation }) => {
           navigation.navigate('Square');
         }}
       />
+      <Button
+        title='Go to Text Demo'
+        onPress={() => {
+          navigation.navigate('Text');
+        }}
+      />
     </View>
   );
 };
